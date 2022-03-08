14136cf3eaa3383688c8cc29f712d19cf9acb9f8
Mariajossdeny <mariajossdeny@gmail.com>
2022-03-08 14:47:31 -0600

    cambios en el diseño

--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
  import validator from './validator.js';
 
+/* Funcion para la galeria de fotos */
+ const slider = document.querySelector("#slider");
+ let sliderSection = document.querySelectorAll(".slider-section");
+ let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+ const btnLeft = document.querySelector("#btn-left");
+ const btnRight = document.querySelector("#btn-right");
+ slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+    function next() {//hacia la derecha
+    let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
+    slider.style.marginleft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function () {
+       slider.style.transition = "none";
+       slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+       slider.style.marginleft = "-100%"
+    }, 500); 
+
+    }
+
+    function prev() { //hacia la izquierda
+    let sliderSection = document.querySelectorAll(".slider-section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginleft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function () {
+       slider.style.transition = "none"
+       slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+       slider.style.marginleft = "-100%";
+       //(prev)
+    }, 500);
+   }
+ 
+
+ btnRight.addEventListener("click", function() {
+    next();
+ });
+
+ btnLeft.addEventListener("click", function() {
+    console.log("hola")
+    prev();
+ });
+
+
       //seccion donde el usuario puede ver mas detalles de su destino preferido:
 //opcion1(Houston-texas)
 document.getElementById("option1").addEventListener("click",() => {
